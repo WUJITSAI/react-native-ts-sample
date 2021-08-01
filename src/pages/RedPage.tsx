def43d9314005099c8c6d7a842498c4549318be6
ariontechs <nat@ariontechs.com>
2021-08-01 11:17:39 +0800

    Added tab to naivgater

--- src/pages/RedPage.tsx
+++ src/pages/RedPage.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Button, StyleSheet} from 'react-native';
+import { useNavigation } from '@react-navigation/native'
 
 export default () => {
+    const navigation = useNavigation()
+
     return (
-        <View style={{flex:1, backgroundColor:'red'}} />
+        <View style={styles.container} >
+            <Button color="white" title="PressMe" onPress={() => navigation.navigate('GREEN')}/>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1, 
+        backgroundColor:'red', 
+        justifyContent:'center', 
+        alignItems:'center'
+    }
+})
--- src/pages/YellowPage.tsx
+++ src/pages/YellowPage.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Button, StyleSheet} from 'react-native';
+import { useNavigation } from '@react-navigation/native'
 
 export default () => {
+    const navigation = useNavigation()
+
     return (
-        <View style={{flex:1, backgroundColor:'yellow'}} />
+        <View style={styles.container} >
+            <Button color="white" title="BACK" onPress={() => navigation.goBack()}/>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1, 
+        backgroundColor:'yellow', 
+        justifyContent:'center', 
+        alignItems:'center'
+    }
+})
