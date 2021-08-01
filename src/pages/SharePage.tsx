5606afa52de9b5e3fa1c448718229add5ac669c0
ariontechs <nat@ariontechs.com>
2021-08-01 15:58:54 +0800

    update share module

--- src/pages/SharePage.tsx
+++ src/pages/SharePage.tsx
@@ -1,13 +1,79 @@
 import React from 'react';
-import {View, Button, StyleSheet} from 'react-native';
+import {View, Button, StyleSheet, Platform} from 'react-native';
 import { useNavigation } from '@react-navigation/native'
+import Share, {Options} from 'react-native-share';
+
+const getOptions = (): Options => {
+    const url = 'https://awesome.contents.com/';
+    const title = 'Awesome Contents';
+    const message = 'Please check this out.';
+    const icon = 'data:<data_type>/<file_extension>;base64,<base64_data>';
+    return Platform.select({
+        ios: {
+            activityItemSources: [
+                {
+                    // For sharing url with custom title.
+                    placeholderItem: { type: 'url', content: url },
+                    item: {
+                    default: { type: 'url', content: url },
+                    },
+                    subject: {
+                    default: title,
+                    },
+                    linkMetadata: { originalUrl: url, url, title },
+                },
+                {
+                    // For sharing text.
+                    placeholderItem: { type: 'text', content: message },
+                    item: {
+                    default: { type: 'text', content: message },
+                    message: null, // Specify no text to share via Messages app.
+                    },
+                    linkMetadata: {
+                    // For showing app icon on share preview.
+                    title: message,
+                    },
+                },
+                {
+                    // For using custom icon instead of default text icon at share preview when sharing with message.
+                    placeholderItem: {
+                    type: 'url',
+                    content: icon,
+                    },
+                    item: {
+                    default: {
+                        type: 'text',
+                        content: `${message} ${url}`,
+                    },
+                    },
+                    linkMetadata: {
+                    title: message,
+                    icon: icon,
+                    },
+                },
+            ],
+        },
+        default: {
+            title,
+            subject: title,
+            message: `${message} ${url}`,
+        },
+    }); 
+}
+
+const onSharePress = async () => {
+    
+
+    await Share.open(getOptions());
+}
+
 
 export default () => {
     const navigation = useNavigation()
 
     return (
         <View style={styles.container} >
-            <Button title="SHARE" onPress={() => {}}/>
+            <Button title="SHARE" onPress={onSharePress}/>
         </View>
     )
 }
