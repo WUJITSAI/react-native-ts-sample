8ce2f770b360fb2a1e9a1471bcff1afd49be4091
Kenji <kenji@ariontechs.com>
2021-08-01 15:58:25 +0800

    更新 blue page

--- src/pages/BluePage.tsx
+++ src/pages/BluePage.tsx
@@ -1,22 +1,78 @@
 import React from 'react';
-import {View, Button, StyleSheet} from 'react-native';
+import { View, StyleSheet, Text, FlatList, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native'
+
 
 export default () => {
     const navigation = useNavigation()
+    const [data, setData] = React.useState([{
+        name:'AAa',
+        age:30,
+    }])
+
+    const onAddPress = () => {
+        console.log('aaaa');
+        
+        data.push({
+            age: 30,
+            name: 'KK',
+        })
+        setData([...data])
+    }
+
+    const renderMyItem = ({ name }: { name: string, age: number }) => {
+        return (
+            <View style={{ backgroundColor: 'red' }}>
+                <Text>{name}</Text>
+            </View>
+        )
+    }
+
 
     return (
         <View style={styles.container} >
-            <Button color="white" title="PressMe" onPress={() => navigation.navigate('RED')}/>
+            <FlatList 
+            data={data} 
+            renderItem={(item) => renderMyItem(item.item)} />
+            
+            <TouchableOpacity style={styles.touchableOpacityPorsition} onPress={onAddPress} >
+                <View style={styles.touchableOpacityView} />
+            </TouchableOpacity>
+            <View style={styles.flatListView}></View>
         </View>
     )
 }
 
+
+
+// const Item = ({title}) =>{
+//     return 
+// }
+
 const styles = StyleSheet.create({
     container: {
-        flex:1, 
-        backgroundColor:'blue', 
-        justifyContent:'center', 
-        alignItems:'center'
+        flex: 1,
+        backgroundColor: 'blue',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        color: 'white',
+        fontSize: 30,
+    },
+    flatListView: {
+        backgroundColor: 'white',
+        height: 10,
+    },
+    touchableOpacityView: {
+        width: 50,
+        height: 50,
+        backgroundColor: 'black',
+        borderRadius: 25,
+    },
+    touchableOpacityPorsition: {
+        position: "absolute",
+        right: 20,
+        bottom: 20,
     }
 })
